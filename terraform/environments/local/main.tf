775bb8527ed08667b53a63fae317ab6ae85ae121
mitro <dmtr.melnik@gmail.com>
2024-12-09 12:02:02 +0500

    [Terraform] remove app abstraction

--- terraform/environments/local/main.tf
+++ terraform/environments/local/main.tf
@@ -1,4 +1,37 @@
-module "observability" {
-  source = "../../apps/observability"
+module "docker_network" {
+  source = "../../modules/docker-network"
+  providers = {
+    docker = docker
+  }
+}
+
+locals {
   name_prefix = "logging-benchmarks-"
+}
+
+module "elastic_stack" {
+  source = "../../modules/elastic-stack"
+  providers = {
+    docker = docker
+  }
+  network_name = module.docker_network.name
+  name_prefix = local.name_prefix
+}
+
+module "grafana_stack" {
+  source = "../../modules/grafana-stack"
+  providers = {
+    docker = docker
+  }
+  network_name = module.docker_network.name
+  name_prefix = local.name_prefix
+}
+
+module "postgresql" {
+  source = "../../modules/postgresql"
+  providers = {
+    docker = docker
+  }
+  network_name = module.docker_network.name
+  name_prefix = local.name_prefix
 }
